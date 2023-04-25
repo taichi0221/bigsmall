27410034d767495f88bfc747e8f79063105e2586
taichi0221 <127753483+taichi0221@users.noreply.github.com>
2023-04-26 00:39:30 +0900

    move

--- frontend/src/SameValueComponent.tsx
+++ frontend/src/SameValueComponent.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './SameValueComponent.css';
 
 type SameValueComponentProps = {
@@ -9,9 +9,48 @@ type SameValueComponentProps = {
 function SameValueComponent(props: SameValueComponentProps) {
   const { value, count } = props;
   const fontSize = 10 * count;
+  const [animationStyle, setAnimationStyle] = useState({});
+
+  useEffect(() => {
+    // 画面サイズに合わせて、移動範囲を調整するパーセンテージ
+    const rangePercentage = 80;
+
+    const randomX = (Math.random() * rangePercentage) - rangePercentage / 2;
+    const randomY = (Math.random() * rangePercentage) - rangePercentage / 2;
+    // const randomX = Math.random() * 100 - 50;
+    // const randomY = Math.random() * 100 - 50;
+
+    const animationKeyframes = `@keyframes randomMove {
+      0%, 100% {
+        transform: translate(0, 0);
+      }
+      50% {
+        transform: translate(${randomX}%, ${randomY}%);
+      }
+    }`;
+
+    setAnimationStyle({
+      animation: `randomMove 5s infinite`,
+      WebkitAnimation: `randomMove 5s infinite`,
+    });
+
+    const styleElement = document.createElement('style');
+    styleElement.textContent = animationKeyframes;
+    document.head.appendChild(styleElement);
+
+    return () => {
+      document.head.removeChild(styleElement);
+    };
+  }, []);
 
   return (
-    <div className="same-value-component" style={{ fontSize: `${fontSize}px` }}>
+    <div
+      className="same-value-component"
+      style={{
+        fontSize: `${fontSize}px`,
+        ...animationStyle,
+      }}
+    >
       {value}
     </div>
   );
